f4e4a5ad74dea4965653eccc176106b8c7d8ac5c
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-09-05 23:53:27 +1000

    Add bootstrap theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,10 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery.min
+//= require bootstrap.min
+//= require bootstrap-datepicker
+//= require material.min
+//= require material-kit
+//= require nouislider.min
 //= require_tree .
